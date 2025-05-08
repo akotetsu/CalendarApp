dbb59c8410780a98b29fb892f38482de05352f1b
akotetsu <yuanliqu@gmail.com>
2025-05-08 20:07:18 +0900

    Initial Commit

---
+++ CalenderApp/CalenderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalenderAppApp.swift
+//  CalenderApp
+//
+//  Created by 原里駆 on 2025/05/08.
+//
+
+import SwiftUI
+
+@main
+struct CalenderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
